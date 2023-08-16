6f8143b60089af7bb72fed88235c4aed0bd461ff
Eslam Fareed <eslammando778899@gmail.com>
2023-08-16 12:24:07 +0400

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/Post.java
@@ -0,0 +1,24 @@
+package com.example.myapplication;
+
+public class Post {
+    private String title;
+    private String content;
+    private String image;
+
+    public Post(String title, String content,String image) {
+        this.title = title;
+        this.content = content;
+        this.image = image;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+    public String getImage() {
+        return image;
+    }
+}
